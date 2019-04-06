6bc65123174808491b085366aa69d542a40ac2b6
lazward <lazwerd@gmail.com>
2019-04-06 00:38:35 -0400

    very good progress!
    - learned a bit of sqlite
    - got description setting working

--- bot.js
+++ bot.js
@@ -9,13 +9,27 @@ const client = new Discord.Client({disableEveryone: true}) ;
 const SQLite = require("better-sqlite3") ;
 const sql = new SQLite('./profiles.sqlite') ;
 
-client.on("ready", async () => {
+client.on("ready", () => {
+
+  const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'profiles';") ;
+  if (!table['count(*)']) {
+
+    //sql.prepare("CREATE TABLE IF NOT EXISTS profiles (id TEXT PRIMARY KEY, user TEXT, guild TEXT, title TEXT, desc TEXT);").run() ;
+    //sql.prepare("CREATE UNIQUE INDEX idx_profiles_id ON profiles (id);").run() ;
+    sql.pragma("synchronous = 1") ;
+    sql.pragma("journal_mode = wal") ;
+
+  }
+
+  client.getProfile = sql.prepare("SELECT * FROM profiles WHERE user = ? AND guild = ?") ;
+  client.setProfile = sql.prepare("INSERT OR REPLACE INTO profiles (id, user, guild, title, desc) VALUES (@id, @user, @guild, @title, @desc);") ;
+
 
   console.log("bot has started!") ;
 
 }) ;
 
-client.on("message", async message => {
+client.on("message", message => {
 
   if(message.author.bot) {
 
@@ -41,15 +55,74 @@ client.on("message", async message => {
 
   if (command === `${prefix}userinfo`) {
 
+    let profile = client.getProfile.get(message.author.id, message.guild.id) ;
+
+    if (!profile) {
+
+      profile = buildProfile(message.author.id, message.guild.id) ;
+
+    }
+
     let embed = new Discord.RichEmbed()
       .setAuthor(message.member.displayName, message.author.avatarURL)
-      .setDescription("Test")
-      .addField("Discord Username", `${message.author.username}#${message.author.discriminator}`) ;
+      .setDescription(profile.title)
+      .addField("Description", profile.desc)
+      .addField("Discord Username", `${message.author.username}#${message.author
+      .discriminator}`) ;
+
 
     message.channel.send(embed) ;
 
   }
 
+  if (command === `${prefix}setdesc`) {
+
+    console.log("aaa") ;
+    let member = message.guild.members.find('displayName', args[0]) ;
+    if (member) {
+
+      console.log(member.displayName) ;
+      let newDesc = args.slice(1).join(" ") ;
+      console.log(newDesc) ;
+
+      let profile = client.getProfile.get(message.author.id, message.guild.id) ;
+
+      if (!profile) {
+
+        profile = buildProfile(message.author.id, message.guild.id) ;
+
+      }
+
+      profile.desc = newDesc ;
+
+      client.setProfile.run(profile) ;
+
+
+    } else {
+
+        message.channel.send("User not found!") ;
+
+    }
+
+
+  }
+
 }) ;
+
+function buildProfile(user, guild) {
+
+    let output = {
+
+              id: `${guild}-${user}`,
+              user: user,
+              guild: guild,
+              title: "test",
+              desc: "tesssst"
+
+          }
+
+  return output ;
+
+}
 
 client.login(config.token) ;
